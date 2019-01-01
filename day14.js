6a938b3b7d411a3070d5732498c87ce71abba62e
Marko Schulz <Marko.Schulz@gmail.com>
2019-01-01 20:59:03 +0100

    14: Solve part 2

--- day14.js
+++ day14.js
@@ -120,7 +120,104 @@ test('acceptance of part 1', () => {
   expect(part1(2018)).toBe('5941429882')
 })
 
+const equalRecipes = (recipePos, sought) => {
+  let current = recipePos
+  let result = true
+  sought.split('').forEach(char => {
+    if (Number(char) !== current.value) result = false
+    current = current.next
+  })
+  return result
+}
+
+const containsAtEnd = (recipes, soughtRecipes) => {
+  if (soughtRecipes.length > recipes.last.length) return false
+
+  recipes.seeker = recipes.seeker == null ? recipes.first : recipes.seeker.next
+
+  let found = equalRecipes(recipes.seeker, soughtRecipes)
+
+  if (!found && recipes.seeker.length + soughtRecipes.length <= recipes.last.length) {
+    recipes.seeker = recipes.seeker.next
+    found = equalRecipes(recipes.seeker, soughtRecipes)
+  }
+
+  return found
+}
+
+describe('containsAtEnd', () => {
+  it('return false and does not place a seeker for too short recipe lists', () => {
+    const recipes = toRecipes('37')
+    expect(containsAtEnd(recipes, '123')).toBe(false)
+    expect(recipes.seeker).toBeUndefined()
+  })
+
+  it('sets the seeker at the start', () => {
+    const recipes = toRecipes('123')
+    containsAtEnd(recipes, '123')
+    expect(recipes.seeker).toBe(recipes.first)
+  })
+
+  it('finds a string at the seeker position at the end', () => {
+    const recipes = toRecipes('1234')
+    recipes.seeker = recipes.first
+
+    expect(containsAtEnd(recipes, '234')).toBe(true)
+    expect(recipes.seeker).toBe(recipes.first.next)
+  })
+
+  it('finds a string at the seeker position before the end', () => {
+    const recipes = toRecipes('12345')
+    recipes.seeker = recipes.first
+
+    expect(containsAtEnd(recipes, '234')).toBe(true)
+    expect(recipes.seeker).toBe(recipes.first.next)
+  })
+
+  it('finds a string after the seeker position at the end', () => {
+    const recipes = toRecipes('12345')
+    recipes.seeker = recipes.first
+
+    expect(containsAtEnd(recipes, '345')).toBe(true)
+    expect(recipes.seeker).toBe(recipes.first.next.next)
+  })
+
+  it('progesses the seeker', () => {
+    const recipes = toRecipes('1234')
+    recipes.seeker = recipes.first
+
+    containsAtEnd(recipes, '999')
+    expect(recipes.seeker).toBe(recipes.first.next)
+  })
+
+  it('progesses the seeker multiple times', () => {
+    const recipes = toRecipes('1234')
+    recipes.seeker = recipes.first
+
+    containsAtEnd(recipes, '99')
+    expect(recipes.seeker).toBe(recipes.first.next.next)
+  })
+})
+
+const part2 = (soughtRecipes) => {
+  let recipes = toRecipes('37')
+  let positions = initialPositions(recipes)
+  while (!containsAtEnd(recipes, soughtRecipes)) {
+    nextRecipes(recipes, positions)
+  }
+
+  return recipes.seeker.length - 1
+}
+
+test('acceptance of part 2', () => {
+  expect(part2('51589')).toBe(9)
+  expect(part2('01245')).toBe(5)
+  expect(part2('92510')).toBe(18)
+  expect(part2('59414')).toBe(2018)
+})
+
 if (process.env.NODE_ENV !== 'test') {
-  const inputOfPart1 = 580741
-  console.log('Part 1: ' + part1(inputOfPart1))
+  const input = 580741
+  console.log('Part 1: ' + part1(input))
+  console.log('Part 2: ' + part2(String(input)))
 }
